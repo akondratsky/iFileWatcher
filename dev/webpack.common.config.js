2a1f56346476d1c1aa9177152323df3073dd969c
akondratsky <akondratsky@yandex.ru>
2019-07-31 09:29:08 +0400

    playing with modal window

--- dev/webpack.common.config.js
+++ dev/webpack.common.config.js
@@ -4,16 +4,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     renderer: path.resolve(__dirname, '../src/renderer/app.js'),
-    main: path.resolve(__dirname, '../src/main/index.js')
+    main: path.resolve(__dirname, '../src/main/index.js'),
   },
   resolve: {
     alias: {
-      'Components': path.resolve(__dirname, '../src/renderer/components'),
-      'Pages': path.resolve(__dirname, '../src/renderer/pages'),  
-      'Actions': path.resolve(__dirname, '../src/renderer/actions'),  
-      'Reducers': path.resolve(__dirname, '../src/renderer/reducers'),
-      'Selectors': path.resolve(__dirname, '../src/renderer/selectors')
-    }
+      Components: path.resolve(__dirname, '../src/renderer/components'),
+      Pages: path.resolve(__dirname, '../src/renderer/pages'),
+      Actions: path.resolve(__dirname, '../src/renderer/actions'),
+      Reducers: path.resolve(__dirname, '../src/renderer/reducers'),
+      Selectors: path.resolve(__dirname, '../src/renderer/selectors'),
+    },
   },
   module: {
     rules: [
@@ -36,7 +36,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'dev/index.html',
       title: 'MyApp',
-      inject: false
-    })
-  ]
-}
+      inject: false,
+    }),
+  ],
+};
--- src/renderer/pages/JsonPage/Controls/ControlsView.js
+++ src/renderer/pages/JsonPage/Controls/ControlsView.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import { Button, Modal, Paper } from '@material-ui/core';
+import electron from 'electron';
+const BrowserWindow = electron.remote.BrowserWindow;
+
+const onClick = () => {
+  const child = new BrowserWindow({
+    parent: electron.remote.getCurrentWindow(),
+    modal: true,
+    show: false,
+  });
+  child.loadURL('./myfile.html');
+  child.once('ready-to-show', () => {
+    child.show();
+  });
+};
+
+let isOpened = true;
 
 export const ControlsView = () => (
-  <Button color="default" variant="contained" size="small" onClick={() => console.log('clicked')}>
-    Add watcher
-  </Button>
+  <Fragment>
+    <Button
+      color="default"
+      variant="contained"
+      size="small"
+      onClick={() => {
+        console.log(isOpened);
+        isOpened = !isOpened;
+      }}>
+      Add watcher
+    </Button>
+    <Modal open={isOpened}>
+      <Paper>I am modal window</Paper>
+    </Modal>
+  </Fragment>
 );
