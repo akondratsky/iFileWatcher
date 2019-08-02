f6353219677c10235f83a0d7617b4cb03dc1810a
akondratsky <akondratsky@yandex.ru>
2019-08-02 22:16:59 +0400

    test refactored

--- src/renderer/reducers/watchers.spec.js
+++ src/renderer/reducers/watchers.spec.js
@@ -1,18 +1,43 @@
 import reducer from './watchers';
 import { ActionTypes } from 'Actions/watchers';
 
+const watcherStub1 = {
+  id: 42,
+  enabled: true,
+  name: 'new watcher',
+  file: 'c:\\git\\my\\package.json',
+  task: 'npm run watch',
+  notify: true,
+  install: true,
+  script: true,
+};
+
+const watcherStub2 = {
+  id: 13,
+  enabled: true,
+  name: 'new watcher 3',
+  file: 'd:\\nofolder\\package.json',
+  task: 'npm start',
+  notify: false,
+  install: true,
+  script: false,
+};
+
+// const watcherStubs = [watcherStub1, watcherStub2];
+
+
 describe('Watchers reducers', () => {
-  it('should handle creating new watcher', () => {
+  it('should add watcher if id not exists', () => {
     const action = {
       type: ActionTypes.JSON_SAVE_WATCHER,
       payload: {
-        id: 42,
-        name: 'new reducer',
-        notify: true,
+        ...watcherStub2,
       },
     };
-    const test = [{ ...action.payload }];
-    const actual = reducer([], action);
-    expect(actual).to.deep.equal(test);
+
+    const actual = reducer([{ ...watcherStub1 }], action);
+    const correct = [{ ...watcherStub1 }, { ...watcherStub2 }];
+
+    expect(actual).to.deep.equal(correct);
   });
 });
