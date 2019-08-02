c0480f3c9391f03d6748430a2e06f778c6283d1e
akondratsky <akondratsky@yandex.ru>
2019-08-03 02:18:51 +0400

    added typeface-roboto font (default for material-ui)

--- dev/webpack.common.config.js
+++ dev/webpack.common.config.js
@@ -22,14 +22,18 @@ module.exports = {
         use: [{ loader: 'babel-loader' }],
       },
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(jpe?g|png|gif)$/,
         use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
-      }
-    ]
+      },
+    ],
   },
   target: 'electron-renderer',
   plugins: [
