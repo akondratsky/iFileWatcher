c3038dd7922c122107d26117cae02741ad27fa85
akondratsky <akondratsky@yandex.ru>
2019-08-03 01:20:15 +0400

    added action (green)

--- src/renderer/actions/watcherEditor.js
+++ src/renderer/actions/watcherEditor.js
@@ -1,3 +1,10 @@
+import { createAction } from 'redux-actions';
+
 export const ActionTypes = {
   WATCHEREDITOR_SET_WATCHER_EDITOR_OPENED: 'WATCHEREDITOR_SET_WATCHER_EDITOR_OPENED',
-}
+};
+
+export const setWatcherEditorOpened = createAction(
+  ActionTypes.WATCHEREDITOR_SET_WATCHER_EDITOR_OPENED,
+  (isOpened) => isOpened,
+);
