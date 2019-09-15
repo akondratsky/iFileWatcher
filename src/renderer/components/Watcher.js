4bb5e92a41bd8ff653a88fd19ab9a1ec39d124c9
Aleksandr-Kondratskii <aleksandr_kondratskii@epam.com>
2019-09-15 15:12:27 +0400

    just saving progress

--- src/renderer/components/Watcher.js
+++ src/renderer/components/Watcher.js
@@ -6,24 +6,32 @@ import debounce from 'debounce';
 export class Watcher {
   constructor(watcher) {
     this.id = watcher.id;
-    this.filename = watcher.file;
-    this.fsWatcher = fs.watch(this.filename, {}, debounce(this.changeHandler, 300));
-
-    console.log(`watcher ${this.id} was created`);
-    // const our_watcher = getWatcherById(getState(), { id: this.id });
+    this.watchForFile(watcher.file);
   }
 
-  changeHandler(eventType, filename) {
+  debouncedChangeHandler = debounce(this.changeHandler, 300);
+
+  changeHandler = (eventType, filename) => {
     console.log(eventType);
     console.log(`file changed: ${filename}`);
-  }
-
-  update(watcher) {
-    console.log(`watcher ${this.id} was updated`);
-  }
+  };
 
   stop() {
     this.fsWatcher.close();
     console.log(`watcher ${this.id} was stopped`);
+  }
+
+  update(watcher) {
+    if (this.file !== watcher.file) {
+      this.fsWatcher.close();
+      this.watchForFile(watcher.file);
+    }
+
+    console.log(`watcher ${this.id} was updated`);
+  }
+
+  watchForFile(filename) {
+    this.file = filename;
+    this.fsWatcher = fs.watch(filename, {}, this.debouncedChangeHandler);
   }
 }
